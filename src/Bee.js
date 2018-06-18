23ad5aa331064e1a13893a3c5b1d645b617181e4
Ricardo <rroque98@gmail.com>
2018-06-18 12:11:12 -0700

    Passes HoneyMakerBee functionality.

--- src/Bee.js
+++ src/Bee.js
@@ -2,9 +2,8 @@ var Bee = function() {
   Grub.call(this);
   this.age = 5;
   this.color = 'yellow'
-  // this.age = 
+  this.job = 'keep on growing';
 };
 
 Bee.prototype = Object.create(Grub.prototype);
 Bee.prototype.constructor = Bee;
-Bee.prototype.job = 'keep on growing';
